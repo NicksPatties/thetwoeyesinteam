39ee167b2110c12b04649c64d856868d02216054
wing <wing.tam@oskoui-oskoui.com>
2013-09-04 17:31:55 -0700

    mutiple img loading sample

--- Dev/Eclipse/workspace/MovieSlam_Android_Dev/src/com/example/movieslam_android_dev/views/SplashPage.java
+++ Dev/Eclipse/workspace/MovieSlam_Android_Dev/src/com/example/movieslam_android_dev/views/SplashPage.java
@@ -135,11 +135,21 @@ public class SplashPage extends Activity implements ResponseDelegate, ImgRequest
 			play_iv1.init("http://graph.facebook.com/639473268/picture?type=large", R.id.playerThumbnail1_iv);
 			play_iv1.execute();
 			
-			/*
+			
 			ImgRequestHandler play_iv2 = new ImgRequestHandler();
 			play_iv2.delegate = this;
 			play_iv2.init("http://screenslam.foxfilm.com/include/images/avatar.png", R.id.playerThumbnail2_iv);
-			play_iv2.execute();*/
+			play_iv2.execute();
+			
+			ImgRequestHandler play_iv3 = new ImgRequestHandler();
+			play_iv3.delegate = this;
+			play_iv3.init("http://graph.facebook.com/1185978290/picture?type=large", R.id.playerThumbnail3_iv);
+			play_iv3.execute();
+			
+			ImgRequestHandler play_iv4 = new ImgRequestHandler();
+			play_iv4.delegate = this;
+			play_iv4.init("http://graph.facebook.com/100000659852503/picture?type=large", R.id.playerThumbnail4_iv);
+			play_iv4.execute();
 			
 			/*
 			// generate challenge boxes
