1e599ec5d829dbaa6f641d05d3a20e1c2a09f09c
wing <wing.tam@oskoui-oskoui.com>
2013-09-12 10:32:53 -0700

    update

--- Dev/Eclipse/workspace/MovieSlam_Android_Dev/src/views/SplashPage.java
+++ Dev/Eclipse/workspace/MovieSlam_Android_Dev/src/views/SplashPage.java
@@ -55,9 +55,9 @@ public class SplashPage extends FragmentActivity implements ResponseDelegate, Co
 		super.onCreate(savedInstanceState); 
         setContentView(R.layout.activity_main);
         
-        startActivity(new Intent(getApplicationContext(), ReadyToPlayPage.class));
+        //startActivity(new Intent(getApplicationContext(), ReadyToPlayPage.class));
         
-        /**
+        
         // init promo image
         final ImageView bg_preloader = (ImageView) findViewById(R.id.bg_preloader);
         new DownloadImageTask(bg_preloader).execute(BASE_URL+"/include/images/screenslam_loading_promo.jpg");
@@ -98,7 +98,7 @@ public class SplashPage extends FragmentActivity implements ResponseDelegate, Co
 		
 		// FB connected
 		//LoginButton loginButton = (LoginButton) user_main_board.findViewById(R.id.loginButton);
-		*/
+		
 	}
 	
 	private String getUIDFromDevice() {
