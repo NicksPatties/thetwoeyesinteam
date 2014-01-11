72267d842aec8603ddc32f16323d000c52c42755
willtconq <ting.j.gan@gmail.com>
2014-01-11 15:49:27 -0800

    EyeManager.cs

---
+++ Assets/Scripts 2/EyeManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeManager : MonoBehaviour {
+
+	private Transform leftEye;
+	private Transform rightEye;
+	
+
+	// Use this for initialization
+	void Start () {
+		leftEye = transform.Find("Left Eye");
+		rightEye = transform.Find("Right Eye");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
