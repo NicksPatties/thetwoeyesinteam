b553c840638abfbf8f69c3ef0d5412098dabe166
Nick Patti <nickspatties@gmail.com>
2014-01-18 17:48:22 -0800

    Implemented score and timer. Next I will implement actions fading in and out, and then getting the pause screen to appear.

---
+++ Assets/Scripts 2/ScoreScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+	
+	int score;
+
+
+	void Start () {
+		score = 0;
+
+		formatGUIText();
+	}
+
+
+	void formatGUIText(){
+		string scoreString = string.Format("{0:D10}", score);
+		gameObject.guiText.text = scoreString;
+	}
+
+
+	/**
+	 * Adds points to the GUI Score text object
+	 * 
+	 * input: int points
+	 * 		the number of points to add to the score
+	 * 
+	 * output: void
+	 * 		changes the GUI Text object to the new score
+	 */
+	public void addPoints(int points){
+
+		score = score + points;
+
+		// format and place the string into GUIText object
+		formatGUIText ();
+	}
+
+
+	/**
+	 * A convenience function that sets the score to a given value.
+	 * 
+	 * input: int points
+	 * 		sets the score to the points value
+	 * 
+	 * output: void
+	 * 		the GUI Text object to is set to points
+	 * 
+	 */ 
+	public void setScore(int points){
+		score = points;
+
+		// format and place the string into GUIText object
+		formatGUIText ();
+	}
+
+
+	void testScoreGUI(){
+	}
+
+
+	void Update () {
+	}
+}
---
+++ Assets/Scripts 2/TimerScript.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour {
+
+	float time;
+	float freakOutThreshold;
+	bool  countingDown;
+ 
+
+	void Start () {
+		time = 0.0f;
+		freakOutThreshold = 3.0f;
+		//countingDown = true;
+	}
+
+
+	/**
+	 * Check if time has expired
+	 * 
+	 * input: none
+	 * 
+	 * output:
+	 * 		true if time is zero, false if there is time remaining
+	 */ 
+	public bool timeHasExpired(){
+		if (time <= 0f)
+			return true;
+		return false;
+	}
+
+
+	void formatTimer(){
+
+		if (time <= 0f) time = 0f;
+
+		// turn the float into a string that looks like xx:xx
+		string timerString = string.Format("{0:00.00}", time);
+		timerString = timerString.Replace('.', ':');
+		gameObject.guiText.text = timerString;
+
+	}
+
+
+	/**
+	 * Adds additional time to the timer
+	 * 
+	 * input: float t
+	 * 		the time to add to the current timer
+	 * 
+	 * output: void
+	 * 		changes the timer to the new value
+	 * 
+	 */ 
+	public void addTime(float t){
+		time = time + t;
+
+		formatTimer();
+	}
+
+
+	/**
+	 * Convenience method to set the timer to a particular value
+	 * 
+	 * input: float t
+	 * 		the timer's new time
+	 * 
+	 * output: void
+	 * 		changes the timer to the new value
+	 */ 
+	public void setTimer(float t){
+		time = t;
+
+		formatTimer ();
+	}
+
+
+	void flashTimer(){
+		gameObject.guiText.color = Color.red;
+	}
+
+
+	void testTimer(){
+
+		// test adding time
+		setTimer(5f);
+	}
+
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+			testTimer ();
+
+		// if time has not been expired
+		if(!timeHasExpired()){
+
+			time = time - Time.deltaTime;
+
+			// if the player is running out of time
+			if(time < freakOutThreshold){
+
+				//flash the timer red
+				flashTimer ();
+			}
+
+			formatTimer();
+
+		}else{
+
+			//TODO: play a "you lose sound only once"
+			/*if(!audio.isPlaying){
+				audio.Play();
+			}*/
+		}
+	}
+}
