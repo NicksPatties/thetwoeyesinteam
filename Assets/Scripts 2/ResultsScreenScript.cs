f3d558e9c540279bb1032df55996ba27f5f854f2
Nick Patti <nickspatties@gmail.com>
2014-01-24 12:59:49 -0800

    Working on results screen UI, but put on hold for sprint re-evaluation.

--- Assets/Scripts 2/ResultsScreenScript.cs
+++ Assets/Scripts 2/ResultsScreenScript.cs
@@ -9,18 +9,42 @@ public class ResultsScreenScript : MonoBehaviour {
 
 
 	//gameobjects of different elements of the results screen
+	GameObject successOrFailMessage;
+	GameObject stevensPortrait;
+	GameObject timeBonusText;
+	GameObject timeBonusIcon;
 	GameObject totalScoreText;
+	GameObject totalScoreIcon;
 
 
 	void Start () {
 		score = 0;
 		remainingTime = 0f;
 
+		successOrFailMessage = GameObject.Find("Success or Fail Message");
+		stevensPortrait = GameObject.Find ("Steven's Portrait");
+		timeBonusText = GameObject.Find ("Time Bonus Text");
 		totalScoreText = GameObject.Find("Total Score Text");
-		totalScoreText.guiText.text = string.Format("{0:D10}", score);
+		totalScoreIcon = GameObject.Find("Total Score Icon");
+
+
+		//testing to see if I'm grabbing the correct game objects
+		print (successOrFailMessage);
+		print (stevensPortrait);
+		print (totalScoreText);
+		print (totalScoreIcon);
+
 	}
 
 
+	/*
+	 * set the score and remaining time values to be used in the results screen
+	 * 
+	 * input: int s (score), float rt (remaining time)
+	 * 
+	 * output: void
+	 * 		assigns the score and remaining time to use for final score calculations
+	 */
 	void setScoreAndRemainingTime(int s, float rt){
 		score = s;
 		remainingTime = rt;
