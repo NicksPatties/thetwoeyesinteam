18fb146310c1ff8c99ebbd2942da6600fae1fe78
Nick Patti <nickspatties@gmail.com>
2014-01-24 11:28:43 -0800

    Rearranged some game objects in the UI Results screen, but not much else.

---
+++ Assets/Scripts 2/ResultsScreenScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultsScreenScript : MonoBehaviour {
+
+	//variables that will be acquired from the gameplay
+	int score;
+	float remainingTime;
+
+
+	//gameobjects of different elements of the results screen
+	GameObject totalScoreText;
+
+
+	void Start () {
+		score = 0;
+		remainingTime = 0f;
+
+		totalScoreText = GameObject.Find("Total Score Text");
+		totalScoreText.guiText.text = string.Format("{0:D10}", score);
+	}
+
+
+	void setScoreAndRemainingTime(int s, float rt){
+		score = s;
+		remainingTime = rt;
+	}
+
+
+	void Update () {
+	
+	}
+}
