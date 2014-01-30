6dd1aba07b19ef90236e60ece86da448dae852ca
Lark <larkyan2788@gmail.com>
2014-01-29 18:11:31 -0800

    add a image for toilet and zipper, but the zipper doesn't show up

--- Assets/Scripts 2/TaskManagerChp3.cs
+++ Assets/Scripts 2/TaskManagerChp3.cs
@@ -5,7 +5,7 @@ public class TaskManagerChp3 : MonoBehaviour {
 	//TODO: Find a way to not have to create Task Managers for each chapter
 
 	//each action is a 2-elements string array, first element is the target object, second is the name of action
-	public string[][] actionList = new string[4][];
+	public string[][] actionList = new string[5][];
 	//a reference for eyemanager to get the current action requirment, 
 	public string[] curAction;
 	//init action index as 0, later we will increase it in order to trigger new action requirment
@@ -22,6 +22,7 @@ public class TaskManagerChp3 : MonoBehaviour {
 		actionList[1] = new string[2]{"pantsfly","find"};
 		actionList[2] = new string[2]{"toilet","focus"};
 		actionList[3] = new string[2]{"pantsfly", "find"};
+		actionList[4] = new string[2]{"done", "none"};
 		curAction = actionList[actionIndex];
 
 		actionPrompt = GameObject.Find("Action Prompt");
