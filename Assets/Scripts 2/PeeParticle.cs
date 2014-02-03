e915168e5b15e5f622e746c7afe375c3801ada02
willtconq <ting.j.gan@gmail.com>
2014-02-02 19:30:36 -0800

    Pee Stream Particles

---
+++ Assets/Scripts 2/PeeParticle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PeeParticle : MonoBehaviour {
+
+	private Vector2 pos;
+	private GameObject self;
+
+	// Use this for initialization
+	void Start () {
+		self = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		self.transform.position = pos;
+	}
+
+	public void setPosition(Vector2 position) {
+		pos.x = position.x;
+		pos.y = position.y;
+	}
+
+	public void setVelocity() {
+
+	}
+
+
+}
--- Assets/Scripts 2/PeeStream.cs
+++ Assets/Scripts 2/PeeStream.cs
@@ -1,24 +1,47 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class PeeStream : MonoBehaviour {
+	
+	public int maxParticles;
+	public float particleVelocity;
 
-	public SpriteRenderer SR;
-	public ParticleSystem PS;
+	private int particleCount;
+	private Transform peeParticle;
+	private Transform target;
+	private List<Transform> pee;
 
 	// Use this for initialization
 	void Start () {
 		//Change Foreground to the layer you want it to display on
 		//You could prob. make a public variable for this
-		particleSystem.renderer.sortingLayerName = "Player";
-		SR = transform.Find("PeeStream").GetComponent<SpriteRenderer>();
-		PS = transform.Find("PeeStream").GetComponents<ParticleSystem>()[1];
+		particleCount = 0;
+		peeParticle = transform.Find("PeeParticle");
+		target = transform.Find ("Cursor");
+		pee = new List<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//if (SR != null && PS != null) {
-		//	for(int i = 0; i < PS.GetParticles(); i++) {
+		if (particleCount < maxParticles) {
+			addParticle();
+		}
+		//foreach (Transform piss in pee) {
 
+		//}
+	}
+
+	void addParticle() {
+		Transform newPeeParticle;
+		newPeeParticle = (Transform) Instantiate(peeParticle);
+		PeeParticle pp = (PeeParticle) newPeeParticle.GetComponent(typeof(PeeParticle));
+		Vector2 pos;
+		pos.x = peeParticle.transform.position.x;
+		pos.y = peeParticle.transform.position.y;
+		pp.setPosition(pos);
+
+
+		pee.Add(newPeeParticle);
+		particleCount++;
 	}
 }
