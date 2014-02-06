6a07eb470ce9eed6f7b6c16eb820e6dcadb92be9
Lark <larkyan2788@gmail.com>
2014-02-05 18:31:10 -0800

    add blur effect code and cheating keys game object

---
+++ Assets/CheatingKeys.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheatingKeys : MonoBehaviour {
+	private bool blurred = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.B)){
+			GameObject mainCamera = GameObject.Find("Main Camera");
+			blurred = !blurred;
+			mainCamera.GetComponent<ChangableBlur>().enabled = blurred;
+		}
+	}
+}
