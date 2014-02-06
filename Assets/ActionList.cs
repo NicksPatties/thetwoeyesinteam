30956b8a07ce9971388564c248429abe9aaf36c7
Lark <larkyan2788@gmail.com>
2014-02-05 17:43:47 -0800

    add an empty file actionlist and import the blur effect from the asset store

---
+++ Assets/ActionList.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionList : MonoBehaviour {
+	public string[][] targetObject;
+	public string[] actionName;
+	public string[] correctness;
+	 	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
