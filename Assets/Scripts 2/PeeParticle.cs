443148c61aa378f1f86e8ac7704ae5c7dd4c5d11
willtconq <ting.j.gan@gmail.com>
2014-02-13 03:28:37 -0800

    I can piss now!

--- Assets/Scripts 2/PeeParticle.cs
+++ Assets/Scripts 2/PeeParticle.cs
@@ -5,34 +5,77 @@ public class PeeParticle : MonoBehaviour {
 
 	private GameObject self;
 	private Vector2 position;
-	public Vector2 velocity;
-	public int blah;
+	public Vector2 targetPosition;
+	public float maxVelocity;
+	public bool original;
+	public GameObject stream;
 
 	// Use this for initialization
 	void Start () {
+		stream = GameObject.Find("PeeStream");
 		self = this.gameObject;
 		position.x = 0;
 		position.y = 0;
-		velocity.x = 0;
-		velocity.y = 0;
-		blah = 0;
+		targetPosition.x = 0;
+		targetPosition.y = 0;
+		maxVelocity = 0;
+		original = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		position.x+= velocity.x;
-		position.y+= velocity.y;
-		self.transform.position = position;
+		//position.x+= velocity.x;
+		//position.y+= velocity.y;
+		//self.transform.position = position;
+		if(!original) {
+			if(maxVelocity == 0)
+				getVelocity();
+			if (targetPosition.x == 0 && targetPosition.y == 0) {
+				getTarget();
+			}
+			if(targetPosition.x == transform.position.x && targetPosition.y == transform.position.y) {
+				resetPosition();
+				getTarget();
+			}
+
+			float step = maxVelocity * Time.deltaTime;
+			transform.position = Vector2.MoveTowards(transform.position, targetPosition, step);
+		}
+	}
+
+	void getVelocity() {
+		if(stream != null) {
+			PeeStream ps = (PeeStream) stream.GetComponent(typeof(PeeStream));
+			if(ps != null)
+				maxVelocity = ps.particleVelocity;
+		}
+	}
+
+	void getTarget() {
+		GameObject target = GameObject.Find("Cursor");
+		if(target != null){
+			targetPosition.x = target.transform.position.x;
+			targetPosition.y = target.transform.position.y;
+		}
+	}
+
+	void resetPosition() {
+		if(stream != null) {
+			Vector2 pos;
+			pos.x = stream.transform.position.x;
+			pos.y = stream.transform.position.y;
+			transform.position = pos;
+		}
+	}
+
+	public void setOriginality() {
+		original = true;
 	}
 
 	public void setPosition(Vector2 pos) {
 		position.x = pos.x;
 		position.y = pos.y;
-	}
-
-	public void setVelocity(Vector2 vel) {
-		velocity.x = vel.x;
-		velocity.y = vel.y;
+		transform.position = position;
 	}
 
 	public void correctRotation() {
@@ -43,6 +86,4 @@ public class PeeParticle : MonoBehaviour {
 		rotate.w = 0;
 		this.transform.rotation = rotate;
 	}
-
-
 }
--- Assets/Scripts 2/PeeStream.cs
+++ Assets/Scripts 2/PeeStream.cs
@@ -5,20 +5,22 @@ public class PeeStream : MonoBehaviour {
 	
 	public int maxParticles;
 	public float particleVelocity;
-	public Vector2 vel;
-	
-	private int particleCount;
-	private Transform peeParticle;
-	private Transform target;
+	public float emissionSpeed;
+
+	private float emissionWaitTimer;
+	public int particleCount;
+	public Transform originalParticle;
+	public GameObject target;
 	private List<Transform> pee;
 
 	// Use this for initialization
 	void Start () {
 		//Change Foreground to the layer you want it to display on
 		//You could prob. make a public variable for this
+		emissionWaitTimer = 0f;
 		particleCount = 0;
-		peeParticle = transform.Find("PeeParticle");
-		target = transform.Find ("Cursor");
+		originalParticle = transform.Find("PeeParticle");
+		target = GameObject.Find("Cursor");
 		pee = new List<Transform>();
 	}
 	
@@ -27,22 +29,28 @@ public class PeeStream : MonoBehaviour {
 		if (particleCount < maxParticles) {
 			addParticle();
 		}
-		//foreach (Transform piss in pee) {
+		PeeParticle temp = (PeeParticle) originalParticle.GetComponent (typeof(PeeParticle));
+		temp.setOriginality();
 
-		//}
+		foreach (Transform piss in pee) {
+			PeeParticle pp = (PeeParticle) piss.GetComponent(typeof(PeeParticle));
+			/*if (pp.maxVelocity == 0)
+				pp.maxVelocity = particleVelocity;
+			if (pp.targetPosition.x == 0 && pp.targetPosition.y == 0) {
+				pp.targetPosition.x = target.transform.position.x;
+				pp.targetPosition.y = target.transform.position.y;
+			}*/
+		}
 	}
 
 	void addParticle() {
 		Transform newPeeParticle;
-		newPeeParticle = (Transform) Instantiate(peeParticle);
+		newPeeParticle = (Transform) Instantiate(originalParticle);
 		PeeParticle pp = (PeeParticle) newPeeParticle.GetComponent(typeof(PeeParticle));
 		Vector2 pos;
-		pos.x = peeParticle.transform.position.x;
-		pos.y = peeParticle.transform.position.y;
+		pos.x = transform.position.x;
+		pos.y = transform.position.y;
 		pp.setPosition(pos);
-		vel.x = 0.01f;
-		vel.y = 0.01f;
-		pp.setVelocity(vel);
 		pp.correctRotation();
 
 		pee.Add(newPeeParticle);
