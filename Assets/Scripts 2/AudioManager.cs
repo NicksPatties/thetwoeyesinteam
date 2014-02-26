139698a70a0dcedafb5e482bd9394ed300a99861
Nick Patti <nickspatties@gmail.com>
2014-02-26 13:52:21 -0800

    Added AudioManager and UIManager files to the game. Will start integrating them to the game soon.

---
+++ Assets/Scripts 2/AudioManager.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	// gameObjects that play audio
+	GameObject background;
+	GameObject background2;
+	GameObject checkmark;
+	GameObject cross;
+	GameObject player;
+
+	AudioSource[] playerVO; //all of the VO in the player object
+	AudioSource[] stevenFindsCorrectObject;
+	AudioSource[] stevenFindsIncorrectObject;
+	AudioSource[] stevenLooksAtObject;
+
+
+	void Start () {
+		//assign the variables to the gameObjects that have the audio sources
+		background  = GameObject.Find("background");
+		background2 = GameObject.Find("background2");
+		checkmark   = GameObject.Find("ui checkmark");
+		cross       = GameObject.Find("ui cross");
+		player	    = GameObject.Find("player");
+
+		playerVO = player.GetComponents<AudioSource>();
+		assignVOBanks();
+
+		//audio sources should be in the gameObject themselves, or in here?
+		//one way to do it:
+		//		have all the VO go to the player
+		//		have all the feedback sounds go to the UI
+		//		have the ambiance stick to the background
+		//		
+		//playing audio:
+		//		if moments are composed of multiple sounds, then have functions
+		//		call them that are placed in here, like "playFoundObject()".
+		//		the sources will be in the objects, but controlling when they do
+		//		or don't play, and thier timing will be easier in here
+
+		//begin playing the background ambiance
+		background.audio.Play();
+	}
+
+
+	//must be careful when doing this!
+	//order of items in playerVO dependent on their placement in the editor!
+	void assignVOBanks(){
+		
+		stevenFindsCorrectObject = new AudioSource[2];
+		stevenFindsCorrectObject[0] = playerVO[0];
+		stevenFindsCorrectObject[1] = playerVO[1];
+		
+		stevenFindsIncorrectObject = new AudioSource[2];
+		stevenFindsIncorrectObject[0] = playerVO[2];
+		stevenFindsIncorrectObject[1] = playerVO[3];
+
+		stevenLooksAtObject = new AudioSource[2];
+		stevenLooksAtObject[0] = playerVO[4];
+		stevenLooksAtObject[1] = playerVO[5];
+	}
+
+
+	//TODO: Lark suggests these functions should also be able to pass in a sound
+	//that we want to play, which is probably a good idea as well
+	void playFoundCorrectObject(){
+		checkmark.audio.Play();
+	
+		//random index
+		int i = Random.Range(0, stevenFindsCorrectObject.Length);
+
+		//play the random VO clip after the checkmark sound plays
+		float d = checkmark.audio.clip.length;
+		stevenFindsCorrectObject[i].PlayDelayed(d * 0.33f);
+	}
+
+
+	void playFoundIncorrectObject(){
+		cross.audio.Play();
+
+		//random index
+		int i = Random.Range(0, stevenFindsIncorrectObject.Length);
+
+		//play the random VO clip after the cross sound plays
+		float d = cross.audio.clip.length;
+		stevenFindsIncorrectObject[i].PlayDelayed(d * 0.33f);
+
+	}
+
+
+	void playLookingAtObject(){
+		int i = Random.Range(0, stevenLooksAtObject.Length);
+		stevenLooksAtObject[i].Play();
+	}
+
+
+	// probably should cange this to a fadein/fadeout, since this function assumes that
+	// both audio files have the same volume
+	IEnumerator changeAmbiance(GameObject oldBackground, GameObject newBackground){
+
+		//set the new background ambiance volume to 0 and begin playing
+		newBackground.audio.volume = 0f;
+		newBackground.audio.Play();
+
+		float currentVolume = oldBackground.audio.volume;
+		for(; currentVolume > 0f; currentVolume -= 0.1f){
+
+			oldBackground.audio.volume = currentVolume;
+			yield return new WaitForSeconds(0.05f);
+		}
+
+	}
+
+
+	void Update () {
+		//testing different functions in here
+		if(Input.GetKeyDown(KeyCode.A)){
+			playFoundCorrectObject();
+		}
+		
+		if(Input.GetKeyDown(KeyCode.S)){
+			playFoundIncorrectObject();
+		}
+		
+		if(Input.GetKeyDown(KeyCode.D)){
+			playLookingAtObject();
+		}
+		
+		if(Input.GetKeyDown(KeyCode.F)){
+			StartCoroutine(changeAmbiance(background, background2));
+		}
+		
+	}
+}
+
+
+
---
+++ Assets/Scripts 2/UIManager.cs
@@ -0,0 +1,313 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	GameObject check;
+	GameObject cross;
+	GameObject great;
+	GameObject nope;
+	GameObject[] allUI; //a convenience array to do actions to all UI elements
+
+	//status bar gameObjects
+	GameObject statusBarBG;
+	GameObject statusBarText;
+	float sbBGMovementAmount;
+	float sbTextMovementAmount;
+	Vector3 sbBGOffscreenPos;
+	Vector3 sbTextOffscreenPos;
+
+	//objects in the game world
+	GameObject sign;
+
+	//animation variables
+	float fadeSpeed;
+	bool  selected;
+	float scaleSize;
+
+
+	// Use this for initialization
+	void Start () {
+		check = GameObject.Find ("ui checkmark");
+		cross = GameObject.Find ("ui cross");
+		great = GameObject.Find ("ui great!");
+		nope  = GameObject.Find ("ui nope");
+		sign  = GameObject.Find ("sign");
+		allUI = new GameObject[4];
+
+		allUI[0] = check;
+		allUI[1] = cross;
+		allUI[2] = great;
+		allUI[3] = nope;
+
+		statusBarBG   = GameObject.Find ("status bar background");
+		statusBarText = GameObject.Find ("text");
+
+		//TODO: you were in the middle of this
+		//status bar original positions: bg = (0, -4.139404, 0), text = (0.5, 0.0682869, 0)
+		sbBGOffscreenPos   = new Vector3(0f, -5.65f, 0f);
+		sbTextOffscreenPos = new Vector3(0.5f, -0.11f, 0f);
+
+		sbBGMovementAmount = 0f;
+		sbTextMovementAmount = 0f;
+
+		fadeSpeed = 0.2f; //smaller number -> slower speed
+		selected  = false;
+		scaleSize = 1.2f; //larger number -> larger size
+
+		makeUIInvisible();
+	}
+
+
+	void makeUIInvisible(){
+		foreach(GameObject go in allUI){
+
+			//change the color of the objects to transparent
+			Color c = go.renderer.material.color;
+			go.renderer.material.color = new Color(c.r, c.g, c.b, 0f);
+		}
+	}
+
+
+	IEnumerator rotate360degrees(GameObject go, float speed, int spins){
+		
+		float time = speed;
+		
+		//do the rotation
+		while(time <= spins * 360){
+			
+			go.transform.Rotate(speed, 0f, 0f);
+			time += speed;
+			yield return new WaitForFixedUpdate();
+		}
+		
+	}
+	
+	
+	IEnumerator fadeIn(GameObject go, float fadeSpeed){
+		
+		// save the original color
+		Color c = go.renderer.material.color;
+		
+		// make it invisible
+		float a = 0f;
+		go.renderer.material.color = new Color(c.r, c.g, c.b, a);
+		
+		// fade the sprite in
+		while(a <= 1f){
+			go.renderer.material.color = new Color(c.r, c.g, c.b, a);
+			a += fadeSpeed;
+			yield return new WaitForFixedUpdate();
+			
+		}
+	}
+	
+	
+	IEnumerator fadeOut(GameObject go, float fadeSpeed){
+
+		//if the go is already invisible, then there's no reason to fadeOut
+		//TODO: any way to solve this issue?
+		if(go.renderer.material.color.a <= 0){
+			print ("This is already invisible!");
+			StopCoroutine("fadeOut"); //something analogous to 'return'
+		}
+		
+		//save the original color
+		Color c = go.renderer.material.color;
+		
+		//make it fully visible
+		float a = 1f;
+		go.renderer.material.color = new Color(c.r, c.g, c.b, a);
+
+		//fade the sprite out
+		while (a >= 0f){
+			go.renderer.material.color = new Color(c.r, c.g, c.b, a);
+			a -= fadeSpeed;
+			yield return new WaitForFixedUpdate();
+			
+		}
+	}
+
+
+	IEnumerator returnToZeroDegrees(GameObject go){
+		float   angle  = 0f;            // just default values
+		Vector3 axis   = Vector3.right; // these two vars will be replaced
+
+		int iterations = 4; //higher number means slower animation
+
+		go.transform.rotation.ToAngleAxis(out angle, out axis);
+		float step = angle/iterations;
+
+
+		while(angle > 0f){
+			go.transform.Rotate (-step, 0f, 0f);
+			angle -= step;
+			yield return new WaitForFixedUpdate();
+		}
+
+	}
+
+
+	/*
+	 * slideStatusBarUpFromBottom
+	 * 		Moves the status bar from off screen up to the bottom of the screen
+	 * 
+	 * input: none - TODO: although... should it take in the status bar game object?
+	 * output: none
+	 */
+	IEnumerable slideStatusBarUpFromBottom(){
+
+		// when it's appearing, add values
+		// TODO: when bar and text are disappearing, subtract
+
+		// in the loop
+		for(float journey = 0.0f; journey <= 1f; journey += 0.2f){
+			//change the location of the bottom bar
+			//change the location of the text
+
+			yield return new WaitForFixedUpdate();
+		}
+			
+	}
+
+
+	/*
+	 * makeNopeUIAppear
+	 * 		Just as it says; the negative feedback at the top center of the screen appears.
+	 * 		Future versions can have a gameObject passed into the funtion
+	 * 
+	 * input: 
+	 * 		go - the GameObject to place in the top center of the screen
+	 * 		positiveFeedback - true for positive, false for negative, makes the UI spin in place
+	 * 
+	 * output: none
+	 */
+	IEnumerator makeUIAppearAtTopCenter(GameObject go, bool positiveFeedback){
+
+		if(positiveFeedback){
+			StartCoroutine(rotate360degrees(go, 40f, 2));
+		}
+
+		//make the sprite appear
+		StartCoroutine(fadeIn (go, fadeSpeed));
+
+		//wait a bit
+		yield return new WaitForSeconds(1.5f);
+
+		//fade the sprite out
+		StartCoroutine(fadeOut(go, fadeSpeed));
+	}
+
+
+	/*
+	 * makeCheckmarkOrCrossAppear
+	 * 		Makes the check or cross UI appear on top of a given position.
+	 * 
+	 * input:
+	 * 		newPosition - the position where the cross or check are going to appear,
+	 * 			ideally the position of the object that is being selected
+	 * 		correctObject - true for correct, false for incorrect, selects which UI to
+	 * 			display on the screen
+	 * 
+	 * output: none
+	 */
+	IEnumerator makeCheckmarkOrCrossAppear(Vector3 newPosition, bool correctObject){
+
+		//select whether to use the X or the Check
+		GameObject go;
+		if(correctObject){
+			go = check;
+		}else{
+			go = cross;
+		}
+
+		//set the location of the checkmark to the input location
+		go.transform.position = newPosition;
+
+		//rotate the checkmark by 95 degrees immediately
+		go.transform.rotation = Quaternion.AngleAxis(90f, Vector3.right);
+
+		//make visible immediately
+		Color c = go.renderer.material.color;
+		go.renderer.material.color = new Color(c.r, c.g, c.b, 1f);
+
+		//rotate it back to 0 degrees
+		StartCoroutine(returnToZeroDegrees(go));
+
+		//wait a little bit
+		yield return new WaitForSeconds(1.5f);
+
+		//fade out 
+		StartCoroutine(fadeOut(go, fadeSpeed));
+	}
+
+
+	/*
+	 * increaseOrDecreaseInSize
+	 * 		Increases or decreases the size of a given gameobject. Uses the 
+	 * 
+	 * input:
+	 * 		go - the gameObject to increase or decrease the size of
+	 * 		isSelected - true if the object is selected by the eyeManager's cursor, false
+	 * 			if not, chooses to increase or decrease the size of the object
+	 */
+	IEnumerator increaseOrDecreaseInSize(GameObject go, bool grow){
+		Vector3 originalSize = go.transform.localScale;
+		Vector3 changedSize  = originalSize;
+		print("originalSize: " + originalSize);
+		
+		if(grow){
+			changedSize *= scaleSize;
+			print ("changedSize after growing: " + changedSize);
+		}else{
+			changedSize /= scaleSize;
+			print ("changedSized after shrinking: " + changedSize);
+		}
+		
+		//this is where the loop is supposed to go
+		for(float journey = 0.0f; journey <= 1.0f; journey += 0.2f){
+			go.transform.localScale = Vector3.Lerp(originalSize, changedSize, journey);
+			yield return new WaitForFixedUpdate();
+		}
+	}
+
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Q)){
+
+			//positive feedback
+			StartCoroutine(makeUIAppearAtTopCenter(great, true));
+		}
+
+		if(Input.GetKeyDown(KeyCode.W)){
+
+			//negative feedback
+			StartCoroutine(makeUIAppearAtTopCenter(nope, false));
+		}
+
+		if(Input.GetKeyDown(KeyCode.E)){
+
+			//make checkmark appear
+			Vector3 pos = sign.transform.position;
+			StartCoroutine(makeCheckmarkOrCrossAppear(pos, true));
+		}
+
+		if(Input.GetKeyDown(KeyCode.R)){
+
+			//make cross appear
+			Vector3 pos = sign.transform.position;
+			StartCoroutine(makeCheckmarkOrCrossAppear(pos, false));
+		}
+
+		if(Input.GetKeyDown(KeyCode.T)){
+			if(selected){
+				selected = false;
+			}else{
+				selected = true;
+			}
+
+			StartCoroutine(increaseOrDecreaseInSize(sign, selected));
+		}
+	
+	}
+}
